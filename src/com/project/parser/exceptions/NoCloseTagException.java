7a2806bd7f42c7c17bb24943707309459761c058
root <kuba5k5@gmail.com>
2020-06-19 16:09:13 +0200

    small changes

--- src/com/project/parser/exceptions/NoCloseTagException.java
+++ src/com/project/parser/exceptions/NoCloseTagException.java
@@ -1,8 +1,5 @@
 package com.project.parser.exceptions;
 
-/**
- * Created by Konrad on 2015-05-18.
- */
 public class NoCloseTagException extends Exception{
     int manyNoCloseTag;
     public NoCloseTagException(int manyNoCloseTag){
